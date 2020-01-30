2c2d877a8c252a23a6ab153cbfb40d667e4950fb
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-30 22:40:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -92,9 +92,9 @@ end
     WHERE name = ?
     SQL
   
-  dog = DB[:conn].execute(sql,name).map do |row|
-  
-    
+  dog = DB[:conn].execute(sql, name).map do |row|
+  self.new_from_db(row)
+end.first
   end 
   
   def update
