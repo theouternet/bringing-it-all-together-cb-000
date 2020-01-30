efa672bee09797af8e511b6d56558f9cbca09894
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-30 19:43:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,55 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :id
+  
+  def initialize(id: nil, name:, breed:)
+    @name = name 
+    @breed = breed
+    @id = id
+  end 
+
+  
+  def self.create_table 
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table 
+    sql
+  end 
+  
+  def save 
+    
+  end 
+  
+  def self.create 
+    
+  end 
+  
+  def self.new_from_db 
+    
+  end 
+  
+  def self.find_by_id
+    
+  end 
+  
+  def self.find_or_create_by
+    
+  end 
+  
+  def self.find_by_name 
+    
+  end 
+  
+  def update
+    
+  end 
+  
+end 
