6a6bf8b9d4e96ed58db42667c35f85c0c67323e2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-30 19:45:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,7 +21,14 @@ class Dog
   end 
   
   def self.drop_table 
-    sql
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
     
     
   end 
