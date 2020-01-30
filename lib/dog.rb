5270fd56160330e49634d3d49f60657896cfc05a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-30 20:15:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,8 +39,8 @@ class Dog
 end 
   end 
   
-  def self.create(:name, :breed)
-    dog = Dog.new(:name, :breed)
+  def self.create(name:, breed:)
+    dog = Dog.new(name:, breed:)
     dog.save
     
   end 
